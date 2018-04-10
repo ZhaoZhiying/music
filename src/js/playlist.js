80e3e214a295192314e457bb6dc544341a67d01a
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-04-10 18:17:16 +0800

    添加点击歌曲列表激活事件

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -9,11 +9,16 @@
             $el.html(this.template)
             //创建 li
             let {songs} = data
-            let liList = songs.map((song)=>{return $('<li></li>').text(song.name)})
+            let liList = songs.map((song)=>{return $('<li></li>').text(song.name).attr('dataSongId' ,song.id)})
             $el.find('ul').empty()
             liList.map((li)=>{
                 $el.find('ul').append(li)
             })
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -38,19 +43,32 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.getAllSongs()
+            this.bindEvents()
+        },
+        getAllSongs(){
+            return this.model.find().then(()=>{
+                console.log(this.model.data)
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click', 'li', (e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('dataSongId')
+                window.eventHub.emit('select', {id: songId})
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload', ()=>{
                 this.view.clearActive()
             })
-            //监听创建 song 事件
+            //监听 newSong 事件
             window.eventHub.on('create', (songData)=>{ 
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
-            this.model.find().then(()=>{
-                console.log(this.model.data)
-                this.view.render(this.model.data)
-            })
-        }
+        },
     }
     controller.init(view, model)
 }
